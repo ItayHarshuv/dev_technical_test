7d17462bcd7bc621b76420f4255f73a06fafa990
Itay Har-shuv <itayhar-shuv@Itays-MacBook-Air.local>
2025-12-28 19:09:47 +0100

    adjust mongo_url name

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/dev_technical_test';
+const MONGODB_URI = process.env.MONGO_URL || process.env.MONGODB_URI || 'mongodb://localhost:27017/dev_technical_test';
 
 export const connectDatabase = async (): Promise<void> => {
   try {
